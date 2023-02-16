1d24905262efdb74287975eedf137bd36df757f7
廖昆 <13126855156@163.com>
2023-02-16 13:56:46 +0800

    1

--- yxwddaye/script/devices.py
+++ yxwddaye/script/devices.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 # -*- encoding=utf8 -*
 __author__ = "windtalker"
+
 import subprocess
 
-result = subprocess.run("adb devices", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output = result.stdout.decode("utf-8")
 
-# 去除输出结果的头部信息
-output = output.split("\n", 1)[-1]
+def get_device():
+    result = subprocess.run("adb devices", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = result.stdout.decode("utf-8")
 
-# 对输出结果进行分割，每一行都是一个设备的信息
-devices = output.strip().split("\n")
+    # 去除输出结果的头部信息
+    output = output.split("\n", 1)[-1]
 
-# 提取出每个设备的ID
-device_ids = [device.split("\t")[0] for device in devices]
+    # 对输出结果进行分割，每一行都是一个设备的信息
+    devices = output.strip().split("\n")
 
-# print(device_ids)
+    # 提取出每个设备的ID
+    device_ids = [device.split("\t")[0] for device in devices]
+    return device_ids
+    # print(device_ids)
